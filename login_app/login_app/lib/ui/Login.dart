6a9d8c22f01b172eab6ae43cf25f38513194f70f
Long Thay <thaybunlong20@yahoo.com>
2018-06-03 20:14:59 +0700

    Add edit text for Login page

--- login_app/login_app/lib/ui/Login.dart
+++ login_app/login_app/lib/ui/Login.dart
@@ -5,19 +5,59 @@ class Login extends StatefulWidget {
   State<StatefulWidget> createState() {
     return new LoginState();
   }
-
 }
 
 class LoginState extends State<Login> {
+
+  final TextEditingController _userController = new TextEditingController();
+  final TextEditingController _passwordController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Login"),
+        centerTitle: true,
         backgroundColor: Colors.orangeAccent,
       ),
       backgroundColor: Colors.orangeAccent.shade100,
+      body: new Container(
+        alignment: Alignment.topCenter,
+        child: new Column(
+          children: <Widget>[
+            new Image.asset(
+              'images/face.png',
+              width: 90.0,
+              height: 90.0,
+              color: Colors.redAccent,
+            ),
+            // form
+            new Container(
+              height: 180.0,
+              width: 380.0,
+              color: Colors.white,
+              child: new Column(
+                children: <Widget>[
+                  new TextField(
+                    controller: _userController,
+                    decoration: new InputDecoration(
+                      hintText: 'Username',
+                      icon: new Icon(Icons.person)
+                    ),
+                  ),
+                  new TextField(
+                    controller: _passwordController,
+                    decoration: new InputDecoration(
+                        hintText: 'Password',
+                        icon: new Icon(Icons.lock)
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
-
 }
