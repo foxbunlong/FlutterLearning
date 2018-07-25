9eaf86c50c4856eec9ef5572c16ad22a7a00fc57
Long Thay <thaybunlong20@yahoo.com>
2018-07-25 14:23:58 +0700

    Add project for Login app

--- login_app/login_app/lib/ui/Login.dart
+++ login_app/login_app/lib/ui/Login.dart
@@ -8,9 +8,27 @@ class Login extends StatefulWidget {
 }
 
 class LoginState extends State<Login> {
-
   final TextEditingController _userController = new TextEditingController();
   final TextEditingController _passwordController = new TextEditingController();
+  String _welcomeString = "";
+
+  void clearText() {
+    setState(() {
+      _userController.clear();
+      _passwordController.clear();
+    });
+  }
+
+  void showWelcome() {
+    setState(() {
+      if (_userController.text.isNotEmpty &&
+          _passwordController.text.isNotEmpty) {
+        _welcomeString = _userController.text;
+      } else {
+        _welcomeString = "Nothing";
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +41,7 @@ class LoginState extends State<Login> {
       backgroundColor: Colors.orangeAccent.shade100,
       body: new Container(
         alignment: Alignment.topCenter,
-        child: new Column(
+        child: new ListView(
           children: <Widget>[
             new Image.asset(
               'images/face.png',
@@ -41,20 +59,68 @@ class LoginState extends State<Login> {
                   new TextField(
                     controller: _userController,
                     decoration: new InputDecoration(
-                      hintText: 'Username',
-                      icon: new Icon(Icons.person)
-                    ),
+                        hintText: 'Username', icon: new Icon(Icons.person)),
                   ),
                   new TextField(
                     controller: _passwordController,
                     decoration: new InputDecoration(
-                        hintText: 'Password',
-                        icon: new Icon(Icons.lock)
-                    ),
+                        hintText: 'Password', icon: new Icon(Icons.lock)),
+                        obscureText: true, // Password type with dots
                   ),
+                  new Padding(padding: new EdgeInsets.all(10.5)),
+                  new Center(
+                    child: new Row(
+                      children: <Widget>[
+                        // Login Button
+                        new Container(
+                          margin: const EdgeInsets.only(left: 38.0),
+                          child: new RaisedButton(
+                            onPressed: showWelcome,
+                            color: Colors.redAccent,
+                            child: new Text(
+                              "Login",
+                              style: new TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.9,
+                              ),
+                            ),
+                          ),
+                        ),
+                        // Clear Button
+                        new Container(
+                          margin: const EdgeInsets.only(left: 120.0),
+                          child: new RaisedButton(
+                            onPressed: clearText,
+                            color: Colors.redAccent,
+                            child: new Text(
+                              "Clear",
+                              style: new TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.9,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
-            )
+            ),
+            new Padding(padding: const EdgeInsets.all(14.0)),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text(
+                  "Welcome, $_welcomeString",
+                  style: new TextStyle(
+                    color: Colors.white,
+                    fontSize: 19.4,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
