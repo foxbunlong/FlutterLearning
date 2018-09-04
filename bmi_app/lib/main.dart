85530d8a82d440243222be014a49f3a5acc8c7e4
Long Thay <thaybunlong20@yahoo.com>
2018-09-04 08:37:22 +0700

    Handle options selected. Not yet add source to calculate BMI

--- bmi_app/lib/main.dart
+++ bmi_app/lib/main.dart
@@ -34,9 +34,20 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  final TextEditingController _ageController = new TextEditingController();
+  final TextEditingController _heightController = new TextEditingController();
+  final TextEditingController _weightController = new TextEditingController();
+
+  void _calculateBMI() {
+    setState(() {
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: new AppBar(
         title: new Text(widget.title),
         centerTitle: true,
@@ -52,6 +63,76 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 85.0,
               width: 75.0,
             ),
+            new Container(
+              margin: const EdgeInsets.all(3.0),
+              height: 310.0,
+              width: 290.0,
+              color: Colors.grey.shade300,
+              child: new Wrap(
+                children: <Widget>[
+                  new TextField(
+                    controller: _ageController,
+                    keyboardType: TextInputType.number,
+                    decoration: new InputDecoration(
+                        labelText: 'Age',
+                        hintText: 'eg. 34',
+                        icon: new Icon(Icons.person_outline)),
+                  ),
+                  new TextField(
+                    controller: _heightController,
+                    keyboardType: TextInputType.number,
+                    decoration: new InputDecoration(
+                        labelText: 'Height in feet',
+                        hintText: 'eg. 6.5',
+                        icon: new Icon(Icons.insert_chart)),
+                  ),
+                  new TextField(
+                    controller: _weightController,
+                    keyboardType: TextInputType.number,
+                    decoration: new InputDecoration(
+                        labelText: 'Weight in lbs',
+                        hintText: 'eg. 180',
+                        icon: new Icon(Icons.line_weight)),
+                  ),
+                  new Padding(padding: new EdgeInsets.all(10.6)),
+                  new Container(
+                    alignment: Alignment.center,
+                    child: new RaisedButton(
+                      onPressed: () => debugPrint('Hello world'),
+                      color: Colors.pinkAccent,
+                      child: new Text('Calculate'),
+                      textColor: Colors.white,
+                    ),
+                  ),
+                  new Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      new Text(
+                        "BMI: ",
+                        style: new TextStyle(
+                          color: Colors.blueAccent,
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 19.9
+                        ),
+                      ),
+
+                      new Padding(padding: const EdgeInsets.all(5.0)),
+
+                      new Text(
+                        "Overweight: ",
+                        style: new TextStyle(
+                            color: Colors.pinkAccent,
+                            fontWeight: FontWeight.w500,
+                            fontStyle: FontStyle.italic,
+                            fontSize: 19.9
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
