0272474615c0ae4153c50a6388eead0b545085f5
Long Thay <thaybunlong20@yahoo.com>
2018-09-04 17:14:23 +0700

    Finalize BMI calculator

--- bmi_app/lib/main.dart
+++ bmi_app/lib/main.dart
@@ -37,10 +37,42 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _ageController = new TextEditingController();
   final TextEditingController _heightController = new TextEditingController();
   final TextEditingController _weightController = new TextEditingController();
+  double inches = 0.0;
+  double result = 0.0;
+  String _resultReading = "";
 
   void _calculateBMI() {
     setState(() {
+      int age = int.parse(_ageController.text);
+      double height = double.parse(_heightController.text);
+      double weight = double.parse(_weightController.text);
 
+      inches = height * 12;
+      if ((_ageController.text.isNotEmpty || age > 0) &&
+          ((_heightController.text.isNotEmpty || inches > 0)) &&
+          ((_weightController.text.isNotEmpty || weight > 0))) {
+
+        result = weight / (inches * inches) * 703;
+        
+        if (double.parse(result.toStringAsFixed(1)) < 18.5) {
+          _resultReading = "Underweight";
+          print(_resultReading);
+        } else if (double.parse(result.toStringAsFixed(1)) >= 18.5
+        && result < 25) {
+          _resultReading = "Great shape!";
+          print(_resultReading);
+        } else if (double.parse(result.toStringAsFixed(1)) >= 25.0
+            && result < 30) {
+          _resultReading = "Overweight";
+          print(_resultReading);
+        } else if (double.parse(result.toStringAsFixed(1)) >= 30.0) {
+          _resultReading = "Obese";
+          print(_resultReading);
+        }
+
+      } else {
+        result = 0.0;
+      }
     });
   }
 
@@ -98,7 +130,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   new Container(
                     alignment: Alignment.center,
                     child: new RaisedButton(
-                      onPressed: () => debugPrint('Hello world'),
+                      onPressed: _calculateBMI,
                       color: Colors.pinkAccent,
                       child: new Text('Calculate'),
                       textColor: Colors.white,
@@ -108,25 +140,21 @@ class _MyHomePageState extends State<MyHomePage> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       new Text(
-                        "BMI: ",
+                        "Your BMI: ${result.toStringAsFixed(1)}",
                         style: new TextStyle(
-                          color: Colors.blueAccent,
-                          fontWeight: FontWeight.w500,
-                          fontStyle: FontStyle.italic,
-                          fontSize: 19.9
-                        ),
+                            color: Colors.blueAccent,
+                            fontWeight: FontWeight.w500,
+                            fontStyle: FontStyle.italic,
+                            fontSize: 19.9),
                       ),
-
                       new Padding(padding: const EdgeInsets.all(5.0)),
-
                       new Text(
-                        "Overweight: ",
+                        "Overweight: $_resultReading",
                         style: new TextStyle(
                             color: Colors.pinkAccent,
                             fontWeight: FontWeight.w500,
                             fontStyle: FontStyle.italic,
-                            fontSize: 19.9
-                        ),
+                            fontSize: 19.9),
                       ),
                     ],
                   ),
