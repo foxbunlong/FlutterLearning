d749a912ff7aae21f5b569139648cc1896fbd605
Long Thay <thaybunlong20@yahoo.com>
2018-09-13 16:18:51 +0700

    Finallize JSONParse project

--- IntroParse/intro_parse/lib/main.dart
+++ IntroParse/intro_parse/lib/main.dart
@@ -3,9 +3,19 @@ import 'dart:async' as async;
 import 'dart:convert' as conv;
 import 'package:http/http.dart' as http;
 
+String _body = "";
+List _data;
+
 void main() async {
-  List _data = await getJson();
-  print(_data[0]);
+  _data = await getJson();
+
+  for (int i = 0; i < _data.length; i++) {
+    print(_data[i]['title']);
+//    print(_data[i]['name']);
+//    print(_data[i]['address']['street']);
+  }
+
+  _body = _data[0]['body'];
 
   runApp(new MyApp());
 }
@@ -54,7 +64,44 @@ class _MyHomePageState extends State<MyHomePage> {
       body: new Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: new Text('Parsing...'),
+        child: new ListView.builder(
+            itemCount: _data.length,
+            padding: const EdgeInsets.all(14.5),
+            itemBuilder: (BuildContext context, int position) {
+              return Column(
+                children: <Widget>[
+                  new Divider(height: 5.5),
+                  new ListTile(
+                    title: Text(
+                      "${_data[position]['title']}",
+                      style: new TextStyle(
+                        fontSize: 17.9,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "${_data[position]['body']}",
+                      style: new TextStyle(
+                        fontSize: 13.9,
+                        color: Colors.grey,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    leading: new CircleAvatar(
+                      backgroundColor: Colors.greenAccent,
+                      child: Text(
+                        "${_data[position]['body'][0]}".toUpperCase(),
+                        style: TextStyle(
+                          fontSize: 16.4,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.orangeAccent,
+                        ),
+                      ),
+                    ),
+                    onTap: () => _onTapMessage(context, _data[position]['body']),
+                  )
+                ],
+              );
+            }),
       ),
       floatingActionButton: new FloatingActionButton(
         onPressed: _incrementCounter,
@@ -65,8 +112,27 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+void _onTapMessage(BuildContext context, String message) {
+  var alert = new AlertDialog(
+    title: Text("My App"),
+    content: Text(message),
+    actions: <Widget>[
+      FlatButton(
+        child: Text("OK"),
+        onPressed: () {
+          Navigator.pop(context); // Show from stack
+        },
+      )
+    ],
+  );
+
+  // showDialog(context: context, child: alert);
+  showDialog(context: context, builder: (context) => alert);
+}
+
 async.Future<List> getJson() async {
   String apiUrl = 'http://jsonplaceholder.typicode.com/posts';
+//  String apiUrl = 'http://jsonplaceholder.typicode.com/users';
 
   http.Response response = await http.get(apiUrl);
 
