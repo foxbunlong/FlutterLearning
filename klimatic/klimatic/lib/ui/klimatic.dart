3d885fda9841f8d1b078a496d3d30160b95f2425
Long Thay <thaybunlong20@yahoo.com>
2018-11-06 11:13:48 +0700

    Finallize Klimatic project

--- klimatic/klimatic/lib/ui/klimatic.dart
+++ klimatic/klimatic/lib/ui/klimatic.dart
@@ -12,6 +12,21 @@ class Klimatic extends StatefulWidget {
 }
 
 class _KlimaticState extends State<Klimatic> {
+  String _cityEntered;
+
+  Future _gotoNextScreen(BuildContext context) async {
+    Map results = await Navigator
+        .of(context)
+        .push(new MaterialPageRoute<Map>(builder: (BuildContext context) {
+      return new ChangeCity();
+    }));
+
+    if (results != null && results.containsKey('enter')) {
+      print(results['enter'].toString());
+      _cityEntered = results['enter'];
+    }
+  }
+
   void showStuff() async {
     Map data = await getWeather(utils.appId, utils.defaultCity);
     print(data.toString());
@@ -28,7 +43,7 @@ class _KlimaticState extends State<Klimatic> {
           new IconButton(
             icon: new Icon(Icons.menu),
             onPressed: () {
-              showStuff();
+              _gotoNextScreen(context);
             },
           )
         ],
@@ -47,7 +62,7 @@ class _KlimaticState extends State<Klimatic> {
             alignment: Alignment.topRight,
             margin: const EdgeInsets.fromLTRB(0.0, 10.9, 20.9, 0.0),
             child: new Text(
-              'Spokane',
+              '${_cityEntered == null ? utils.defaultCity : _cityEntered}',
               style: cityStyle(),
             ),
           ),
@@ -56,8 +71,7 @@ class _KlimaticState extends State<Klimatic> {
             child: new Image.asset('images/light_rain.png'),
           ),
           new Container(
-            margin: const EdgeInsets.fromLTRB(30.0, 290.0, 0.0, 0.0),
-            child: updateTempWidget('Beira'),
+            child: updateTempWidget(_cityEntered),
           )
         ],
       ),
@@ -79,19 +93,31 @@ class _KlimaticState extends State<Klimatic> {
   Widget updateTempWidget(String city) {
     // Allow to build future widget after fetch data
     return new FutureBuilder(
-        future: getWeather(utils.appId, city),
+        future:
+            getWeather(utils.appId, city == null ? utils.defaultCity : city),
         builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
           // where we get all the info, setup widget
           if (snapshot.hasData) {
             Map content = snapshot.data;
             return new Container(
+              margin: const EdgeInsets.fromLTRB(30.0, 250.0, 0.0, 0.0),
               child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   new ListTile(
+                    title: new Text(
+                      content['main']['temp'].toString() + " F",
+                      style: tempStyle(),
+                    ),
+                    subtitle: new ListTile(
                       title: new Text(
-                    content['main']['temp'].toString(),
-                    style: tempStyle(),
-                  ))
+                        "Humidity: ${content['main']['humidity'].toString()}\n"
+                            "Min: ${content['main']['temp_min'].toString()} F\n"
+                            "Max: ${content['main']['temp_max'].toString()} F",
+                        style: extraData(),
+                      ),
+                    ),
+                  )
                 ],
               ),
             );
@@ -102,11 +128,70 @@ class _KlimaticState extends State<Klimatic> {
   }
 }
 
+class ChangeCity extends StatelessWidget {
+  var _cityFieldController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        title: new Text('Change city'),
+        centerTitle: true,
+      ),
+      body: new Stack(
+        children: <Widget>[
+          new Center(
+            child: new Image.asset(
+              'images/white_snow.png',
+              width: 490.0,
+              height: 1200.0,
+              fit: BoxFit.fill,
+            ),
+          ),
+          new ListView(
+            children: <Widget>[
+              new ListTile(
+                title: new TextField(
+                  decoration: new InputDecoration(
+                    hintText: 'Enter City',
+                  ),
+                  controller: _cityFieldController,
+                  keyboardType: TextInputType.text,
+                ),
+              ),
+              new ListTile(
+                title: new FlatButton(
+                  onPressed: () {
+                    Navigator
+                        .pop(context, {'enter': _cityFieldController.text});
+                  },
+                  textColor: Colors.white70,
+                  color: Colors.redAccent,
+                  child: new Text('Get weather'),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
 TextStyle cityStyle() {
   return new TextStyle(
     color: Colors.white,
     fontSize: 22.9,
     fontStyle: FontStyle.italic,
+  );
+}
+
+TextStyle extraData() {
+  return new TextStyle(
+    color: Colors.white70,
+    fontSize: 17.0,
+    fontStyle: FontStyle.normal,
   );
 }
 
