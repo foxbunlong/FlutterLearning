9cadadd6e026e86e73a93b46b6fc431ce9dc2c7b
Long Thay <thaybunlong20@yahoo.com>
2020-12-04 16:08:40 +0700

    Add source for iOS flutter channel handling.

--- demo_flutter_channel/demo_flutter_channel/ios/Runner/AppDelegate.swift
+++ demo_flutter_channel/demo_flutter_channel/ios/Runner/AppDelegate.swift
@@ -7,7 +7,25 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let batteryChannel = FlutterMethodChannel(name: "samples.flutter.dev/battery",
+                                                binaryMessenger: controller.binaryMessenger)
+      batteryChannel.setMethodCallHandler({
+        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+          // Note: this method is invoked on the UI thread.
+          guard call.method == "getBatteryLevel" else {
+            result(FlutterMethodNotImplemented)
+            return
+          }
+          self?.receiveBatteryLevel(result: result)
+      })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  private func receiveBatteryLevel(result: FlutterResult) {
+    result(Int.random(in: 0..<101))
+  }
+
 }
+
